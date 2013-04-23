c694142b407ba448d676ac033da41d76b4bcd15d
Cedric Pradalier <cedric.pradalier@gmail.com>
2013-04-23 16:38:33 +0200

    Added mission to test compass calibration

---
+++ sim_tasks/missions/test_calib_compass.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# ROS specific imports
+import roslib; roslib.load_manifest('sim_tasks')
+import rospy
+from math import *
+from task_manager_lib.TaskClient import *
+
+rospy.init_node('task_client')
+server_node = rospy.get_param("~server","/sim_tasks")
+default_period = rospy.get_param("~period",0.2)
+tc = TaskClient(server_node,default_period)
+
+tc.WaitForAuto()
+try:
+    tc.CalibrateCompass(angular_velocity=0.3)
+
+except TaskException, e:
+    rospy.logerr("Exception caught: " + str(e))
+
+if not rospy.core.is_shutdown():
+    tc.SetManual()
+
+
+rospy.loginfo("Mission completed")
+
+
