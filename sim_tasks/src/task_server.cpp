34e2ebd300b95a0f9e819fc37c266f0afb68fd11
Cedric Pradalier <cedric.pradalier@gmail.com>
2013-04-26 16:31:29 +0200

    Added a service to reload the tasks without restarting the server. Also tested compilation with integrated dynamic reconfigure servers

--- sim_tasks/src/task_server.cpp
+++ sim_tasks/src/task_server.cpp
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <signal.h>
 
+#include <std_srvs/Empty.h>
+
 #include "task_manager_lib/SequenceTask.h"
 #include "task_manager_lib/TaskScheduler.h"
 
@@ -19,23 +21,55 @@
 using namespace sim_tasks;
 using namespace task_manager_lib;
 
+
+class TaskServer {
+    protected:
+        ros::NodeHandle nh;
+        ros::ServiceServer service;
+        std::string lib_path;
+        boost::shared_ptr<TaskEnvironment> env;
+        boost::shared_ptr<TaskDefinition> idle;
+        boost::shared_ptr<TaskDefinition> wait;
+        TaskScheduler ts;
+
+        void reloadTasks() {
+            ROS_INFO("Terminating all tasks");
+            ts.terminateAllTasks();
+            ts.stopScheduler();
+            ROS_INFO("Clearing all dynamic tasks");
+            ts.clearAllDynamicTasks();
+            ROS_INFO("Reloading all dynamic tasks");
+            ts.loadAllTasks(lib_path,env);
+            ts.configureTasks();
+            ts.printTaskDirectory(true);
+            ts.startScheduler();
+            ROS_INFO("Reload completed");
+        }
+
+        bool reloadSrv(std_srvs::Empty::Request  &req,
+                std_srvs::Empty::Response &res)
+        {
+            reloadTasks();
+            return true;
+        }
+
+    public:
+        TaskServer() : nh("~"), lib_path("./lib"), env(new SimTasksEnv(nh)), idle(new TaskIdleDefault(env)), wait(new TaskWaitDefault(env)), ts(nh, idle, 0.5) {
+            nh.getParam("lib_path",lib_path);
+            service = nh.advertiseService("reload_tasks", &TaskServer::reloadSrv,this);
+            ts.addTask(wait);
+            ts.loadAllTasks(lib_path,env);
+            ts.configureTasks();
+            ts.printTaskDirectory(true);
+            ts.startScheduler();
+        }
+
+};
+
 int main(int argc, char *argv[])
 {
     ros::init(argc,argv,"sim_tasks");//init ros
-    ros::NodeHandle nh("~");//initialize node
-    std::string lib_path = "./lib";
-    nh.getParam("lib_path",lib_path);
-
-    boost::shared_ptr<TaskEnvironment> env(new SimTasksEnv(nh));
-    boost::shared_ptr<TaskDefinition> idle(new TaskIdleDefault(env));
-    boost::shared_ptr<TaskDefinition> wait(new TaskWaitDefault(env));
-    TaskScheduler ts(nh, idle, 0.5);
-    // TaskServerInterface tsi(ts);
-    ts.addTask(wait);
-    ts.loadAllTasks(lib_path,env);
-    ts.configureTasks();
-    ts.printTaskDirectory(true);
-    ts.startScheduler();
+    TaskServer ts;
     ros::spin();
     return 0;
 }
