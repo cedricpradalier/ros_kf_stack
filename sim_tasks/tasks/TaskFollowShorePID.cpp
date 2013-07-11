bd6f5ceb36b23b7c8d6c75fcf40fdfffed87cd08
Kingfisher Base Station <kingfisher@kingfisher-base.(none)>
2013-07-11 14:21:48 +0200

    Added saturation to FollowShorePID and camera should look at shore continuously.

--- sim_tasks/tasks/TaskFollowShorePID.cpp
+++ sim_tasks/tasks/TaskFollowShorePID.cpp
@@ -37,7 +37,6 @@ TaskIndicator TaskFollowShorePID::iterate()
     geometry_msgs::Vector3 angle;
     const geometry_msgs::Pose2D & tpose = env->getPose2D();
     const geometry_msgs::Pose2D & finishLine = env->getFinishLine2D();
-
     double scalarProduct = cos(finishLine.theta)*(finishLine.x-tpose.x)+sin(finishLine.theta)*(finishLine.y-tpose.y);
     double r = hypot(finishLine.y-tpose.y,finishLine.x-tpose.x);
 
@@ -59,14 +58,16 @@ TaskIndicator TaskFollowShorePID::iterate()
     const pcl::PointCloud<pcl::PointXYZ> & pointCloud = env->getPointCloud();
 
     double vel = cfg.velocity;
-    double rot = 0;
-
     float mindistance=1000;
     float theta_closest=0;
     float distance_i=0;
     float theta_i=0;
-    float i_max = 10.0;
-    float d_max = 5.0;
+    float E_lin_max = cfg.E_lin_max;
+    float E_ang_max = cfg.E_ang_max;
+    float rot_ang = 0.0;
+    float rot_lin = 0.0;
+    float rot = 0.0;
+
 
     for (unsigned int i=0;i<pointCloud.size();i++) {
         theta_i=-atan2(pointCloud[i].y,pointCloud[i].x);
@@ -100,28 +101,18 @@ TaskIndicator TaskFollowShorePID::iterate()
     } else {
 	// Error Calculations - 6 calculations, position error, deriv of position error and int of position error
 	// then the same for the angular error, deriv of angle error and int of angle error.
+	// P error
         angle_error = remainder(cfg.angle-theta_closest,2*M_PI);
         distance_error = cfg.distance-mindistance;
 
+	// D Error
         d_angle_error = (angle_error-angle_error_prev)/cfg.task_period;
         d_distance_error = (distance_error-distance_error_prev)/cfg.task_period;
-	// saturate D's
-	if (fabs(d_angle_error) > d_max) {
-            d_angle_error = ((d_angle_error>0)?+1:-1) * d_max;
-        }
-	if (fabs(d_distance_error) > d_max) {
-            d_distance_error = ((d_distance_error>0)?+1:-1) * d_max;
-        }
 
+	// I error
         i_angle_error += cfg.task_period * angle_error;
         i_distance_error += cfg.task_period * distance_error;
-	// saturate I's
-	if (fabs(i_angle_error) > i_max) {
-            i_angle_error = ((i_angle_error>0)?+1:-1) * i_max;
-        }
-	if (fabs(i_distance_error) > i_max) {
-            i_distance_error = ((i_distance_error>0)?+1:-1) * i_max;
-        }
+
 	// Publish PID errors
 	dist.x=distance_error;
 	dist.y=d_distance_error;
@@ -129,13 +120,29 @@ TaskIndicator TaskFollowShorePID::iterate()
 	angle.x=angle_error;
 	angle.y=d_angle_error;
 	angle.z=i_angle_error;
+
+	// Calculate angular component
+	rot_ang = - cfg.p_alpha * angle_error - cfg.d_alpha * d_angle_error - cfg.i_alpha * i_angle_error;
+	// Calculate linear component
+	rot_lin = - ((cfg.angle>0)?+1:-1) * cfg.p_d * distance_error - cfg.d_d * d_distance_error - cfg.i_d * i_distance_error;
+
+	// Saturate components
+	if (fabs(rot_ang) > E_ang_max) {
+            rot_ang = ((rot_ang>0)?+1:-1) * E_ang_max;
+        }
+	if (fabs(rot_lin) > E_lin_max) {
+            rot_lin = ((rot_lin>0)?+1:-1) * E_lin_max;
+        }	
+	
 	// Rotation calculation
-        rot = - cfg.p_alpha * angle_error - ((cfg.angle>0)?+1:-1) * cfg.p_d * distance_error - cfg.d_alpha * d_angle_error - cfg.d_d * d_distance_error - cfg.i_alpha * i_angle_error - cfg.i_d * i_distance_error;
+        rot = rot_ang + rot_lin;
+
 	// Record errors as previous values for d calculation
 	angle_error_prev = angle_error;
 	distance_error_prev = distance_error;
 	// Saturation and Curvature Limitation
-	vel = exp(-cfg.K_E*((std::max)(0.0,rot*rot-cfg.rot_tol))) * cfg.max_lin_vel;
+	float wnorm =std::max(0.0,fabs(rot)-cfg.rot_tol); 
+	vel = exp(-cfg.K_E*(wnorm*wnorm)) * cfg.max_lin_vel;
         if (fabs(rot) > cfg.max_ang_vel) {
             rot = ((rot>0)?+1:-1) * cfg.max_ang_vel;
         }
